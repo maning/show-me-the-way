95129537e642b11b514c0fc97d580383aa1eeb24
Maning Sambale <emmanuel.sambale@gmail.com>
2013-04-17 16:25:53 +0800

    Update site.js

--- js/site.js
+++ js/site.js
@@ -22,7 +22,7 @@ var paused = false,
         attribution: 'Map data © OpenStreetMap contributors'
     }).addTo(overview_map),
 
-    changesToShowEveryMinute = 20,
+    changesToShowEveryMinute = 10,
 
     oldLine = L.polyline([], {
         opacity: 0.3
